3bf167bce032b02502e0dbef803fb09ab92423a9
javaConductor <javaconductor@yahoo.com>
2019-12-10 13:50:29 -0600

    Fixed news updates

--- src/screens/news/post-list.js
+++ src/screens/news/post-list.js
@@ -5,7 +5,7 @@ import storage from '../../services/storage';
 import styles from '../../screens/main-styles';
 import ShowPost from './show-post';
 import util from "../../services/util";
-
+//import config from '../../config';
 
 export default class PostList extends React.Component {
 	static navigationOptions = {
@@ -19,11 +19,18 @@ export default class PostList extends React.Component {
 		}
 	}
 
+	async checkForNewPosts() {
+		const lastRead = await storage.getNewsPostsLastReadDate();
+		if (lastRead.valueOf() > this.state.lastRead) {
+			console.log(`PostList: checkForNewPosts: lastRead changed ${lastRead.toISOString()}.`);
+			await this.componentDidMount();
+		}
+	}
+
 	componentDidMount = async () => {
 		console.log(`PostList: componentDidMount loading posts.`);
-
 		return storage.getNewsPosts()
-			.then((posts) => {
+			.then(async (posts) => {
 				posts = posts.sort((a, b) => {
 					return (a.date.valueOf() < b.date.valueOf()) ? 1 : -1;
 				});
@@ -34,20 +41,35 @@ export default class PostList extends React.Component {
 					isLoading: false,
 					dataSource: ds.cloneWithRows(posts)
 				};
+				const lastRead = await storage.getNewsPostsLastReadDate();
 				this.setState((prevState) => {
-					return {...prevState, ...otherState}
-				})
+					return {...prevState, ...otherState, lastRead}
+				});
+
+				if (!this.state.interval) {
+					const interval = setInterval(this.checkForNewPosts.bind(this), (30 * 1000));
+					this.setState((prevState) => {
+						return {...prevState, interval}
+					});
+				}
 			})
 			.catch((error) => {
 				console.error(`PostList: componentDidMount: Error loading posts: ERROR: ${util.errorMessage(error)}`);
-
 				throw error;
 			})
 	};
 
+	componentWillUnmount() {
+		this.setState((prevState) => {
+			return {...prevState, interval: null}
+		});
+	}
+
 	render() {
 		if (this.state.isLoading)
 			return null;
+		//console.log(`PostList: render: ${JSON.stringify(this.state.newsPosts,null,2)}.`);
+		//console.log(`PostList: render: newPosts: ${this.state.newsPosts.length } dataSource: ${this.state.dataSource.getRowCount()}`);
 
 		return (
 			<Fragment>
@@ -66,6 +88,6 @@ export default class PostList extends React.Component {
 	}
 
 	renderPost(post) {
-		return <ShowPost post={post}/>
+		return <ShowPost key={post.id} post={post}/>
 	}
 }
