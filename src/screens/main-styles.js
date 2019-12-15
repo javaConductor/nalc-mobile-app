a6d0c98426a067212dde708560074d6608c32e2b
Kraby <krabymarketing@gmail.com>
2019-12-15 02:55:32 -0600

    Update main-styles.js
    Adding padding to content, left & right 10
    
    Co-Authored-By: Kraby <krabymarketing@users.noreply.github.com>

--- src/screens/main-styles.js
+++ src/screens/main-styles.js
@@ -5,8 +5,8 @@ import Constants from 'expo-constants';
 const styles = StyleSheet.create({
 	container: {
 		marginTop: Constants.statusBarHeight,
-		marginLeft: 10,
-		marginRight: 10,
+		marginLeft: 0,
+		marginRight: 0,
 		flex: 1,
 		justifyContent: 'space-around',
 		//alignItems: 'center',
